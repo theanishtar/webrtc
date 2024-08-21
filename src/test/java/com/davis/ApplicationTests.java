4e672cd22a88050d5ef400e4f58f1cfe0b176160
theanishtar <dangtt135@gmail.com>
2024-08-21 11:02:48 +0700

    create ssl cert

---
+++ src/test/java/com/davis/ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.davis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@SpringBootConfiguration
+@ContextConfiguration
+class ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
